061f38388d5066dec7196633d47873941b41d74e
aboudev <aboudev@users.noreply.github.com>
2018-01-17 19:49:32 +0800

    shape detection convex hull rendering

--- Shape_detection.cpp
+++ Shape_detection.cpp
@@ -9,44 +9,32 @@
 #include <iostream>
 #include <fstream>
 
-#ifdef _WIN32
-#include <windows.h>
-#endif
-#include <gl/gl.h>
-
 #include <CGAL/IO/read_xyz_points.h>
 #include <CGAL/Point_with_normal_3.h>
 #include <CGAL/property_map.h>
 #include <CGAL/Shape_detection_3.h>
 #include <CGAL/regularize_planes.h>
-#include <CGAL/Timer.h>
+#include <CGAL/convex_hull_2.h>
 
-// slicer
-#include <CGAL/boost/graph/graph_traits_Surface_mesh.h>
-#include <CGAL/AABB_halfedge_graph_segment_primitive.h>
-#include <CGAL/AABB_tree.h>
-#include <CGAL/AABB_traits.h>
-#include <CGAL/Polygon_mesh_slicer.h>
-
-typedef CGAL::Polyhedron_3<Kernel2> Epic_Polyhedron;
-typedef std::vector<Kernel2::Point_3> Polyline_type;
-typedef std::list<Polyline_type> Polylines;
-typedef CGAL::AABB_halfedge_graph_segment_primitive<Epic_Polyhedron> HGSP;
-typedef CGAL::AABB_traits<Kernel2, HGSP> AABB_traits;
-typedef CGAL::AABB_tree<AABB_traits> AABB_tree;
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#include <gl/gl.h>
 
 #include "Color_256.h"
 
 namespace Algs {
 
-Shape_detection::Shape_detection() :
-  m_dm(m_ransac),
-  // no default constructor for shape_range, although invalid
-  m_bestshapes(m_ransac.shapes()) {}
-
 void Shape_detection::detect(const std::string &fname, const Params::Shape_detection &params)
 {
-  std::cout << "Shape detection...";
+  typedef CGAL::First_of_pair_property_map<Point_with_normal>  Point_map;
+  typedef CGAL::Second_of_pair_property_map<Point_with_normal> Normal_map;
+  // In Shape_detection_traits the basic types, i.e., Point and Vector types
+  // as well as iterator type and property maps, are defined.
+  typedef CGAL::Shape_detection_3::Shape_detection_traits
+    <Kernel2, Pwn_vector, Point_map, Normal_map>               Traits;
+  typedef CGAL::Shape_detection_3::Efficient_RANSAC<Traits>    Efficient_ransac;
+  typedef CGAL::Shape_detection_3::Plane<Traits>               RansacPlane;
 
   m_points.clear();
   // Loads point set from a file. 
@@ -75,91 +63,37 @@ void Shape_detection::detect(const std::string &fname, const Params::Shape_detec
   // 0.9 < dot(surface_normal, point_normal); 
   parameters.normal_threshold = params.normal_threshold;
 
-  // Instantiates shape detection engine.
-  // Provides the input data.
-  m_ransac.set_input(m_points);
-  // Registers planar shapes via template method.
-  m_ransac.add_shape_factory<RansacPlane>();
-  //// Detects registered shapes with default parameters.
-  //m_ransac.detect();
-  //// Prints number of detected shapes.
-  //std::cout << m_ransac.shapes().end() - m_ransac.shapes().begin() << " shapes detected." << std::endl;
+  std::cout << "Shape detection...";
 
-  // Measures time before setting up the shape detection.
-  CGAL::Timer time;
-  time.start();
-  // Build internal data structures.
-  m_ransac.preprocess();
-  // Measures time after preprocessing.
-  time.stop();
-  std::cout << "preprocessing took: " << time.time() * 1000 << "ms" << std::endl;
-  // Perform detection several times and choose result with highest coverage.
-  Efficient_ransac::Shape_range shapes = m_ransac.shapes();
-  FT best_coverage = 0;
-  for (size_t i = 0; i < 6; i++) {
-    // Reset timer.
-    time.reset();
-    time.start();
-    // Detects shapes.
-    m_ransac.detect(parameters);
-    // Measures time after detection.
-    time.stop();
-    // Compute coverage, i.e. ratio of the points assigned to a shape.
-    FT coverage = FT(m_points.size() - m_ransac.number_of_unassigned_points())
-      / FT(m_points.size());
-    // Prints number of assigned shapes and unsassigned points.
-    std::cout << "time: " << time.time() * 1000 << "ms" << std::endl;
-    std::cout << m_ransac.shapes().end() - m_ransac.shapes().begin() << " primitives, "
-      << coverage << " coverage" << std::endl;
+  Efficient_ransac ransac;
+  ransac.set_input(m_points);
+  ransac.add_shape_factory<RansacPlane>();
+  ransac.preprocess();
+  ransac.detect(parameters);
 
-    std::size_t cnt = 0;
-    for (auto &s : m_ransac.shapes()) {
-      //std::cout << s->info() << std::endl;
-      //std::cout << s->indices_of_assigned_points().size() << std::endl;
-      cnt += s->indices_of_assigned_points().size();
-    }
-    std::cout << m_points.size() << " = " << cnt << " + " << m_ransac.number_of_unassigned_points() << std::endl;
+  Efficient_ransac::Shape_range shapes = ransac.shapes();
+  FT coverage = FT(m_points.size() - ransac.number_of_unassigned_points()) / FT(m_points.size());
+  // Prints number of assigned shapes and unassigned points.
+  std::cout << shapes.size() << " primitives, "
+    << coverage << " coverage" << std::endl;
 
-    // Choose result with highest coverage.
-    if (coverage > best_coverage) {
-      best_coverage = coverage;
-      // Efficient_ransac::shapes() provides
-      // an iterator range to the detected shapes. 
-      shapes = m_ransac.shapes();
-      m_bestshapes = shapes;
-    }
-  }
-
-  Efficient_ransac::Shape_range::iterator it = shapes.begin();
-  while (it != shapes.end()) {
-    boost::shared_ptr<Efficient_ransac::Shape> shape = *it;
-    // Using Shape_base::info() for printing 
-    // the parameters of the detected shape.
-    std::cout << (*it)->info();
-    // Sums distances of points to detected shapes.
+  m_point_shapes = std::vector<int>(m_points.size(), -1);
+  int sidx = 0;
+  for (const auto s : shapes) {
     FT sum_distances = 0;
-    // Iterates through point indices assigned to each detected shape.
-    std::vector<std::size_t>::const_iterator
-      index_it = (*it)->indices_of_assigned_points().begin();
-    while (index_it != (*it)->indices_of_assigned_points().end()) {
-
-      // Retrieves point
-      const Point_with_normal &p = *(m_points.begin() + (*index_it));
-      // Adds Euclidean distance between point and shape.
-      sum_distances += CGAL::sqrt((*it)->squared_distance(p.first));
-      // Proceeds with next point.
-      index_it++;
+    for (const std::size_t pidx : s->indices_of_assigned_points()) {
+      sum_distances += CGAL::sqrt(s->squared_distance(m_points[pidx].first));
+      m_point_shapes[pidx] = sidx;
     }
-    // Computes and prints average distance.
-    FT average_distance = sum_distances / shape->indices_of_assigned_points().size();
+
+    FT average_distance = sum_distances / s->indices_of_assigned_points().size();
     std::cout << " average distance: " << average_distance << std::endl;
-    // Proceeds with next detected shape.
-    it++;
+
+    ++sidx;
   }
 
-  // plane regularization in CGAL 4.9
-  // described in "LOD Generation for Urban Scenes", Verdie. et al
-  Efficient_ransac::Plane_range planes = m_ransac.planes();
+  // plane regularization
+  Efficient_ransac::Plane_range planes = ransac.planes();
   CGAL::regularize_planes(m_points,
     Point_map(),
     planes,
@@ -173,141 +107,95 @@ void Shape_detection::detect(const std::string &fname, const Params::Shape_detec
 
   std::cout << "done" << std::endl;
 
-  // m_view_shapes = true;
-
-  std::cout << m_bbox << std::endl;
   if (!m_points.empty()) {
     m_bbox = m_points.begin()->first.bbox();
     for (auto &p : m_points)
       m_bbox = m_bbox + p.first.bbox();
   }
-  std::cout << m_bbox << std::endl;
 
-  return;
+  // rendering data
+  m_convex_hulls.clear();
+  for (const auto s : shapes) {
+    std::list<Kernel2::Point_3> pts;
+    for (const std::size_t &pidx : s->indices_of_assigned_points())
+      pts.push_back(m_points[pidx].first);
+
+    const Kernel2::Plane_3 plane = static_cast<Kernel2::Plane_3>(
+      *dynamic_cast<RansacPlane *>(s.get()));
+    const Kernel2::Point_3 origin = plane.projection(pts.front());
+
+    Kernel2::Vector_3 base1 = plane.base1();
+    Kernel2::Vector_3 base2 = plane.base2();
+    base1 = base1 / std::sqrt(base1.squared_length());
+    base2 = base2 / std::sqrt(base2.squared_length());
+
+    Kernel2::Line_3 baseLine1(origin, base1);
+    Kernel2::Line_3 baseLine2(origin, base2);
+
+    std::vector<Kernel2::Point_2> vec2DCoord;
+    for (const auto &p : pts) {
+        const Kernel2::Point_3 point = plane.projection(p);
+        Kernel2::Vector_3 xVector(origin, baseLine1.projection(point));
+        Kernel2::Vector_3 yVector(origin, baseLine2.projection(point));
+        double x = std::sqrt(xVector.squared_length());
+        double y = std::sqrt(yVector.squared_length());
+        x = xVector * base1 < 0 ? -x : x;
+        y = yVector * base2 < 0 ? -y : y;
+        vec2DCoord.push_back(Kernel2::Point_2(x, y));
+    }
+
+    std::vector<Kernel2::Point_2> cvx_hull_2;
+    CGAL::convex_hull_2(vec2DCoord.begin(), vec2DCoord.end(), std::back_inserter(cvx_hull_2));
+
+    std::vector<Kernel2::Point_3> cvx_hull_3;
+    for (const auto &p : cvx_hull_2)
+      cvx_hull_3.push_back(origin + p.x() * base1 + p.y() * base2);
+
+    m_convex_hulls.push_back(cvx_hull_3);
+  }
+
+  // random color table
+  m_shape_colors = std::vector<std::size_t>(m_convex_hulls.size(), 0);
+  std::srand(static_cast<unsigned int>(std::time(nullptr)));
+  for (std::size_t &c : m_shape_colors)
+    c = static_cast<std::size_t>(std::rand() % 255);
 }
 
 void Shape_detection::draw() {
   if (m_points.empty())
     return;
 
-  Efficient_ransac::Shape_range shapes = m_bestshapes;
-  Efficient_ransac::Shape_range::iterator it = shapes.begin();
-
-  // shape color table
-  std::vector<int> color_tab;
-  for (int i = 0; it != shapes.end(); ++it, ++i) {
-    double R = ((double)i / double(shapes.size())) * (double)255.0;
-    int indiceLut = static_cast<int>(std::floor(R));
-    color_tab.push_back(indiceLut);
-  }
-  std::vector<int>::iterator color_itr = color_tab.begin();
-
-  std::set<std::size_t> pt_idx_set;
-  for (std::size_t i = 0; i < m_points.size(); ++i)
-    pt_idx_set.insert(i);
-
-  ::glDisable(GL_LIGHTING);
-
   // draw point cloud with respect color
+  ::glDisable(GL_LIGHTING);
   ::glPointSize(5.0);
-  it = shapes.begin();
-  while (it != shapes.end()) {
-    ::glColor3ub(Color_256::r(*color_itr), Color_256::g(*color_itr), Color_256::b(*color_itr));
-    ++color_itr;
-    ::glBegin(GL_POINTS);
-    // Iterates through point indices assigned to each detected shape.
-    std::vector<std::size_t>::const_iterator
-      index_it = (*it)->indices_of_assigned_points().begin();
-    while (index_it != (*it)->indices_of_assigned_points().end()) {
-      // Retrieves point
-      const Point_with_normal &p = *(m_points.begin() + (*index_it));
-      ::glVertex3d(p.first.x(), p.first.y(), p.first.z());
-      pt_idx_set.erase(*index_it);
-      // Proceeds with next point.
-      index_it++;
-    }
-    // Proceeds with next detected shape.
-    it++;
-    ::glEnd();
-  }
-  // remaining points
-  std::cout << m_points.size() << " --- " << pt_idx_set.size() << std::endl;
-  ::glColor3f(0.0, 0.0, 0.0);
   ::glBegin(GL_POINTS);
-  for (std::set<std::size_t>::iterator itr = pt_idx_set.begin();
-    itr != pt_idx_set.end(); ++itr)
-    ::glVertex3d(m_points[*itr].first.x(), m_points[*itr].first.y(), m_points[*itr].first.z());
+  for (std::size_t pidx = 0; pidx < m_points.size(); ++pidx) {
+    if (m_point_shapes[pidx] >= 0) {
+      const std::size_t cidx = m_shape_colors[m_point_shapes[pidx]];
+      ::glColor3ub(Color_256::r(cidx), Color_256::g(cidx), Color_256::b(cidx));
+    }
+    else
+      ::glColor3ub(0, 0, 0);
+
+    const Kernel2::Point_3 &p = m_points[pidx].first;
+    ::glVertex3d(p.x(), p.y(), p.z());
+  }
   ::glEnd();
 
-  //::glLineWidth(1.0);
-  //it = shapes.begin();
-  //color_itr = color_tab.begin();
-  //while (it != shapes.end()) {
-  //    ::glColor3ub(Color_256::r(*color_itr), Color_256::g(*color_itr), Color_256::b(*color_itr));
-  //    ++color_itr;
-  //    ::glBegin(GL_LINES);
-  //    // Iterates through point indices assigned to each detected shape.
-  //    std::vector<std::size_t>::const_iterator
-  //        index_it = (*it)->indices_of_assigned_points().begin();
-  //    while (index_it != (*it)->indices_of_assigned_points().end()) {
-  //        // Retrieves point
-  //        const Point_with_normal &p = *(m_points.begin() + (*index_it));
-  //        ::glVertex3d(p.first.x(), p.first.y(), p.first.z());
-  //        ::glVertex3d(p.first.x() + p.second.x() * 0.05, p.first.y() + p.second.y() * 0.05, p.first.z() + p.second.z() * 0.05);
-  //        // Proceeds with next point.
-  //        index_it++;
-  //    }
-  //    // Proceeds with next detected shape.
-  //    it++;
-  //    ::glEnd();
-  //}
-
-  // bounding box to polyhedron
-  /*Epic_Polyhedron bxply = BboxToPolyhedron<Kernel2>(m_bbox);
-  CGAL::Polygon_mesh_slicer<Epic_Polyhedron, Kernel2> slicer(bxply);*/
-  // draw slicer plane
-  glLineWidth(3.0);
-  glColor3f(0.8f, 0.8f, 0.8f);
-  for (auto &s : shapes) {
-    CGAL::Bbox_3 bx = m_points[*(s->indices_of_assigned_points().begin())].first.bbox();
-    for (auto &p : s->indices_of_assigned_points())
-      bx += m_points[p].first.bbox();
-    Epic_Polyhedron bxply = BboxToPolyhedron<Kernel2>(bx);
-    CGAL::Polygon_mesh_slicer<Epic_Polyhedron, Kernel2> slicer(bxply);
-
-    Kernel2::Plane_3 p = shape_info_to_plane(*s);
-    Polylines pls;
-    slicer(p, std::back_inserter(pls));
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-    glBegin(GL_POLYGON);
-    //glBegin(GL_LINE_LOOP);
-    for (auto &pl : pls)
-      for (auto &p : pl)
-        glVertex3d(p.x(), p.y(), p.z());
-    glEnd();
+  // draw convex hull of shape points
+  ::glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  ::glEnable(GL_BLEND);
+  ::glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+  for (std::size_t sidx = 0; sidx < m_convex_hulls.size(); ++sidx) {
+    const auto &cvh = m_convex_hulls[sidx];
+    const std::size_t cidx = m_shape_colors[sidx];
+    ::glColor4ub(Color_256::r(cidx), Color_256::g(cidx), Color_256::b(cidx), 150);
+    ::glBegin(GL_POLYGON);
+    // ::glBegin(GL_LINE_LOOP);
+    for (const auto &p : cvh)
+      ::glVertex3d(p.x(), p.y(), p.z());
+    ::glEnd();
   }
-
-  ::glEnable(GL_LIGHTING);
-}
-
-Kernel2::Plane_3 Shape_detection::shape_info_to_plane(const Efficient_ransac::Shape &s)
-{
-  std::stringstream sstm(s.info());
-  std::string str;
-  sstm >> str;
-  sstm >> str;
-  sstm >> str;
-  double a = std::atof(str.substr(1, str.size() - 2).c_str());
-  sstm >> str;
-  double b = std::atof(str.substr(0, str.size() - 1).c_str());
-  sstm >> str;
-  double c = std::atof(str.substr(0, str.size() - 2).c_str());
-  sstm >> str;
-  sstm >> str;
-  double d = std::atof(str.substr(0, str.size() - 1).c_str());
-
-  //std::cout << a << " " << b << " " << c << " " << d << std::endl;
-  return Kernel2::Plane_3(a, b, c, d);
 }
 
 } // Algs
--- Shape_detection.h
+++ Shape_detection.h
@@ -8,8 +8,6 @@
 #define Scene_shape_detection_H
 
 #include "types.h"
-#include <CGAL/property_map.h>
-#include <CGAL/Shape_detection_3.h>
 
 namespace Algs {
 
@@ -21,29 +19,9 @@ class Shape_detection {
   // Type declarations
   typedef std::pair<Kernel2::Point_3, Kernel2::Vector_3>       Point_with_normal;
   typedef std::vector<Point_with_normal>                       Pwn_vector;
-  typedef CGAL::First_of_pair_property_map<Point_with_normal>  Point_map;
-  typedef CGAL::Second_of_pair_property_map<Point_with_normal> Normal_map;
-  // In Shape_detection_traits the basic types, i.e., Point and Vector types
-  // as well as iterator type and property maps, are defined.
-  typedef CGAL::Shape_detection_3::Shape_detection_traits
-    <Kernel2, Pwn_vector, Point_map, Normal_map>               Traits;
-  typedef CGAL::Shape_detection_3::Efficient_RANSAC<Traits>    Efficient_ransac;
-  typedef CGAL::Shape_detection_3::Plane<Traits>               RansacPlane;
-
-  class Dummy {
-  public:
-    Dummy(Efficient_ransac &r) {
-      std::cerr << "in dummy" << std::endl;
-      r.set_input(Pwn_vector(10, Point_with_normal()));
-      /*r.add_shape_factory<RansacPlane>();
-      r.preprocess();
-      r.detect();*/
-      std::cerr << "dummy done" << std::endl;
-    }
-  };
 
 public:
-  Shape_detection();
+  Shape_detection() {}
 
   const Bbox & bbox() { return m_bbox; }
 
@@ -52,55 +30,17 @@ public:
   void draw();
 
 private:
-  Kernel2::Plane_3 shape_info_to_plane(const Efficient_ransac::Shape &s);
-
-  template <typename K>
-  CGAL::Polyhedron_3<K> BboxToPolyhedron(const CGAL::Bbox_3 &bbox) {
-    typedef typename CGAL::Polyhedron_3<K>::Point_3 Point;
-    typedef typename CGAL::Polyhedron_3<K>::Halfedge_handle Halfedge_handle;
-
-    Point p00(bbox.xmin(), bbox.ymin(), bbox.zmin());
-    Point p01(bbox.xmax(), bbox.ymin(), bbox.zmin());
-    Point p02(bbox.xmax(), bbox.ymax(), bbox.zmin());
-    Point p03(bbox.xmin(), bbox.ymax(), bbox.zmin());
-    Point p10(bbox.xmin(), bbox.ymin(), bbox.zmax());
-    Point p11(bbox.xmax(), bbox.ymin(), bbox.zmax());
-    Point p12(bbox.xmax(), bbox.ymax(), bbox.zmax());
-    Point p13(bbox.xmin(), bbox.ymax(), bbox.zmax());
-
-    CGAL::Polyhedron_3<K> p;
-    Halfedge_handle h = p.make_tetrahedron(p01, p10, p00, p03);
-    Halfedge_handle g = h->next()->opposite()->next();
-    p.split_edge(h->next());
-    p.split_edge(g->next());
-    p.split_edge(g);
-    h->next()->vertex()->point() = p11;
-    g->next()->vertex()->point() = p13;
-    g->opposite()->vertex()->point() = p02;
-    Halfedge_handle f = p.split_facet(g->next(), g->next()->next()->next());
-    Halfedge_handle e = p.split_edge(f);
-    e->vertex()->point() = p12;
-    p.split_facet(e, f->next()->next());
-    CGAL_postcondition(p.is_valid());
-
-    return p;
-  }
-
-
-private:
   Bbox m_bbox;
 
-  // Points with normals.
+  // points with normals.
   Pwn_vector m_points;
+  // shape index of each point
+  std::vector<int> m_point_shapes;
+  // shape color
+  std::vector<std::size_t> m_shape_colors;
 
-  Efficient_ransac m_ransac;
-
-  // dummy class for shapes_range initialization
-  // defined before m_bestshapes
-  // http://stackoverflow.com/questions/4037219/order-of-execution-in-constructor-initialization-list
-  Dummy m_dm;
-
-  Efficient_ransac::Shape_range m_bestshapes;
+  // convex hulls of shape points
+  std::vector<std::vector<Kernel2::Point_3>> m_convex_hulls;
 };
 
 } // namespace Algs
